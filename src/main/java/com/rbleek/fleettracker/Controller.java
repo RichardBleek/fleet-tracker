6351b54b4937db70de3af55dca12719633d6d29f
Richard Bleek <richardbleek@gmail.com>
2020-03-10 11:16:51 +0100

    comment out controller

--- src/main/java/com/rbleek/fleettracker/Controller.java
+++ src/main/java/com/rbleek/fleettracker/Controller.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
-@RestController
-@RequestMapping("/truck")
-public class Controller {
-
-    @GetMapping
-    public Flux<Truck> getTruck() {
-        return Flux.just(new Truck("aa-aa-11"));
-    }
-}
+//@RestController
+//@RequestMapping("/truck")
+//public class Controller {
+//
+//    @GetMapping
+//    public Flux<Truck> getTruck() {
+//        return Flux.just(new Truck("aa-aa-11"));
+//    }
+//}
